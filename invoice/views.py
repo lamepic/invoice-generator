6ae236896cd3a97fc36c40c84abe6af45e58623b
Richard Arthur <richardarthur228@gmail.com>
2021-12-30 02:01:37 +0000

    fixed pdf generator

--- invoice/views.py
+++ invoice/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 
-# from weasyprint import HTML
+from weasyprint import HTML
 
 
 def generate_pdf(request):
+
+    html_string = render_to_string('invoice.html')
+    html = HTML(string=html_string)
+    pdf = html.write_pdf('invoice.pdf')
 
     context = {}
     return render(request, 'invoice.html', context)
